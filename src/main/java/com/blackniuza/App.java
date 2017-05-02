7663c5f4d354d49201cb0d941147abe4c91c9c9d
shiyun.wxm <shiyun.wxm@taobao.com>
2017-05-02 23:50:36 +0800

    Init

---
+++ src/main/java/com/blackniuza/App.java
@@ -0,0 +1,13 @@
+package com.blackniuza;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
